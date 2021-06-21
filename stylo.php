a1b470399ec96b3d068bcd3c3972386295be24c7
aureliem07 <aureliemaillot07@gmail.com>
2021-06-21 15:03:56 +0200

    Ajout classe Stylo.

---
+++ stylo.php
@@ -0,0 +1,37 @@
+<?php
+
+class Stylo
+
+{
+  private $couleur;
+  private $typeMine;
+  
+  public function __construct(string $uneCouleur, string $unTypeMine)
+  {
+    $this->setCouleur($uneCouleur);
+    $this->setTypeMine($unTypeMine);
+  }
+
+  public function getCouleur()
+  {
+    return $this->couleur;
+  }
+
+  public function setCouleur(string $uneCouleur)
+  {
+    $this->couleur = $uneCouleur;
+  }
+
+  public function getTYpeMine()
+  {
+    return $this->typeMine;
+  }
+
+  public function setTypeMine(int $unTypeMine)
+  {
+    $this->typeMine = $unTypeMine;
+  }
+
+}
+
+?>
