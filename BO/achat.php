00bba1372a35e8ac2fad151daf1e8399f425bef8
Vincent NGUYEN <vngnl@laposte.net>
2021-06-21 16:49:58 +0200

    création classe achat git push
    git push
    git pull
    exit

---
+++ BO/achat.php
@@ -0,0 +1,41 @@
+<?php
+
+require_once('ligne.php');
+
+class Achat
+{
+    private $montant;
+    private $lesLignes;
+
+    public function __construct($montant)
+    {
+        $this->montant = $montant;
+    }
+
+    // MONTANT
+
+    public function getMontant()
+    {
+        return $this->montant;
+    }
+
+    public function setMontant($montant)
+    {
+        $this->montant = $montant;
+    }
+
+    // LIGNE
+
+    public function getLignes()
+    {
+        return $this->lesLignes;
+    }
+
+    public function getLigne($index)
+    {
+        if (array_key_exists($index, $this->lesLignes))
+        {
+            return $this->lesLignes[$index];
+        }
+    }
+}
