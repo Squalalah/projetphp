920a7a238443b4d568666966e05654cac5cf252d
Vincent NGUYEN <vngnl@laposte.net>
2021-06-21 15:01:52 +0200

    classe produit

---
+++ produit.php
@@ -0,0 +1,79 @@
+<?php
+
+abstract class Produit
+{
+    protected $refProd;
+    protected $libelle;
+    protected $marque;
+    protected $prixUnitaire;
+    protected $qteStock;
+
+    protected function __construct($refProd, $libelle, $marque, $prixUnitaire, $qteStock)
+    {
+        $this->refProd = $refProd;
+        $this->libelle = $libelle;
+        $this->marque = $marque;
+        $this->prixUnitaire = $prixUnitaire;
+        $this->qteStock = $qteStock;
+    }
+
+    // REFERENCE PRODUIT
+
+    public function getRefprod()
+    {
+        return $this->refProd;
+    }
+
+    public function setRefProd($refProd)
+    {
+        $this->refProd = $refProd;
+    }
+
+    // LIBELLE
+
+    public function getLibelle()
+    {
+        return $this->libelle;
+    }
+
+    public function setLibelle($libelle)
+    {
+        return $this->$libelle;
+    }
+
+    // MARQUE
+
+    public function getMarque()
+    {
+        return $this->marque;
+    }
+
+    public function setMarque($marque)
+    {
+        $this->marque = $marque;
+    }
+
+    // PRIX UNITAIRE
+
+    public function getPrixUnitaire()
+    {
+        return $this->prixUnitaire;
+    }
+
+    public function setPrixUnitaire($prixUnitaire)
+    {
+        $this->prixUnitaire = $prixUnitaire;
+    }
+
+    // QUANTITE STOCK
+
+    public function getQteStock()
+    {
+        return $this->qteStock;
+    }
+
+    public function setQteStock($qteStock)
+    {
+        $this->qteStock = $qteStock;
+    }
+}
