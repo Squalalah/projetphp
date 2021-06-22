a4d60b2caa0fccc4edda38b893555cb5d023ed70
Squalalah <riquezoune@gmail.com>
2021-06-22 15:53:22 +0200

    retrait commentaires inutiles

--- DTO/test/testDTOPain.php
+++ DTO/test/testDTOPain.php
@@ -4,10 +4,6 @@ require_once('../DTOPain.php');
 
 $pain = DTOPain::selectById(1);
 
-
-//private $poids;
-//
-//    public function __construct($dateLimiteConso, $libelle, $marque, $prixUnitaire, $qteStock, $type, $poids, $refProd = '')
 echo 'dateLimiteConso :'.$pain->getDateLimiteConso()."<br>";
 echo 'Libelle :'.$pain->getLibelle()."<br>";
 echo 'Marque :'.$pain->getMarque()."<br>";
