ce55bb3efb5fc83babfdd1a464834be86e230b13
aureliem07 <aureliemaillot07@gmail.com>
2021-06-22 10:04:51 +0200

    Ajout selectByType à DTO Stylo

--- DTO/DTOStylo.php
+++ DTO/DTOStylo.php
@@ -5,27 +5,48 @@ class DTOStylo
 {
 	public static function selectById($refProd)
 	{
-	try {
-		$maCo=self::getBdd();
-	  $req="select * from produit where produit_id=? AND produit_type = 1";
-		$prep=$maCo->prepare($req);
-		$prep->bindParam(1,$refProd,PDO::PARAM_INT); 
-		$prep->execute(); 
-		$mesDataProduit=$prep->fetchObject();
-		$stylo=new Stylo($mesDataProduit->libelle, $mesDataProduit->marque, $mesDataProduit->prixUnitaire, $mesDataProduit->qteStock,
-		$mesDataProduit->couleur,$mesDataProduit->typeMine,$mesDataProduit->refProd);
+		try {
+			$maCo=self::getBdd();
+			$req="select * from produit where produit_id=? AND produit_type = 1";
+			$prep=$maCo->prepare($req);
+			$prep->bindParam(1,$refProd,PDO::PARAM_INT); 
+			$prep->execute(); 
+			$mesDataProduit=$prep->fetchObject();
+			$stylo=new Stylo($mesDataProduit->libelle, $mesDataProduit->marque, $mesDataProduit->prixUnitaire, $mesDataProduit->qteStock,
+			$mesDataProduit->couleur,$mesDataProduit->typeMine,$mesDataProduit->refProd);
 		} 
-	catch (PDOException $e) 
+		catch (PDOException $e) 
 		{
-		echo 'Erreur avec la BD!: ' .$e->getMessage() .'<br/>';
-		die();
+			echo 'Erreur avec la BD!: ' .$e->getMessage() .'<br/>';
+			die();
 		}
-	  return $stylo;
+		return $stylo;
 	}
+
+	public static function selectByType()
+	{
+		try {
+			$maCo=self::getBdd();
+			$req="select * from produit where produit_type = 1";
+			$resultat=$maCo->query($req);
+			while($mesDataProduit=$resultat->fetchObject())
+			{
+				$lesStylos[]=new Stylo($mesDataProduit->libelle, $mesDataProduit->marque, $mesDataProduit->prixUnitaire, $mesDataProduit->qteStock,
+				$mesDataProduit->couleur,$mesDataProduit->typeMine,$mesDataProduit->refProd);	
+			}
+		}
+		catch (PDOException $e) 
+		{
+			echo 'Erreur avec la BD!: ' .$e->getMessage() .'<br/>';
+			die();
+		}
+		return $lesStylos;
+	}
+
 	private static function getBdd() {
 	require('localData.php');
 	return new PDO($dns,$user,$mdp);
-	}
+	}	
 }
 
 ?>
