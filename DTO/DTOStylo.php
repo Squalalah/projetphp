59a8810020be7001316d011b118744e8ad313885
aureliem07 <aureliemaillot07@gmail.com>
2021-06-22 09:14:12 +0200

    Modif DTOStylo

--- DTO/DTOStylo.php
+++ DTO/DTOStylo.php
@@ -1,39 +1,8 @@
 <?php
 
-parent::__construct($libelle, $marque, $prixUnitaire, $qteStock, $refProd = '');
-    $this->setCouleur($couleur);
-    $this->setTypeMine($typeMine);
-
 require_once('Stylo.php');
 class DTOStylo
 {
-	public static function insert(Stylo $data)
-	{
-	try {
-		$maCo=self::getBdd();
-		$req="INSERT INTO produit (libelle, marque, prixUnitaire, qteStock, couleur, typeMine) VALUES (?, ?, ?, ?, ?, ?)";
-		$prep=$maCo->prepare($req);
-		$libelle=$data->getLibelle();
-		$marque=$data->getMarque();
-		$prixUnitaire=$data->getPrixUnitaire();
-		$qteStock=$data->getQteStock();
-		$couleur=$data->getCouleur();
-		$typeMine=$data->getTypeMine();
-		$prep->bindParam(1,$libelle);
-		$prep->bindParam(2,$marque);
-		$prep->bindParam(3,$prixUnitaire);
-		$prep->bindParam(4,$qteStock);
-		$prep->bindParam(5,$couleur);
-		$prep->bindParam(6,$typeMine);
-		$prep->execute();
-		$data->setRefProd($maCo->lastInsertId());
-		} 
-	catch (PDOException $e) 
-		{
-		echo 'Erreur avec la BD!: ' .$e->getMessage() .'<br/>';
-		die();
-		}
-	}
 	public static function selectById($refProd)
 	{
 	try {
