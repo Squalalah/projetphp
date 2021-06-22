657e8bfb8bdc925f879f4764a65601a9f8288a63
Vincent NGUYEN <vngnl@laposte.net>
2021-06-22 12:02:34 +0200

    test carte postale ok

--- DTO/test/testDTOCartePostale.php
+++ DTO/test/testDTOCartePostale.php
@@ -2,15 +2,13 @@
 
 require_once('../DTOCartePostale.php');
 
-$CartePostale = DTOCartePostale::selectById(4);
+// TEST FONCTION SELECTBYID
 
-echo 'test libelle : '. $CartePostale->getLibelle();
-
-
-//$tableau = DTOCartePostale::selectByType();
-
-
+echo 'Test de la fonction "selectById" :<br>';
 echo '<br>';
+
+
+$CartePostale = DTOCartePostale::selectById(4);
 
 echo 'libelle : ' . $CartePostale->getLibelle() . '<br>';
 echo 'marque : ' . $CartePostale->getMarque() . '<br>';
@@ -18,3 +16,23 @@ echo 'prix hunitaire : ' . $CartePostale->getPrixUnitaire() . '<br>';
 echo 'stock : ' . $CartePostale->getQteStock() . '<br>';
 echo 'type de produit : ' . $CartePostale->getType() . '<br>';
 echo 'Id de la carte postale : ' . $CartePostale->getRefProd() . '<br>';
+echo '<br>';
+
+// TEST FONCTION SELECTBYID
+
+echo 'Test de la fonction "selectByType" :<br>';
+echo '<br>';
+
+
+$toutesLesCP = DTOCartePostale::selectByType();
+
+foreach ($toutesLesCP as $carte) {
+    echo 'libelle : ' . $carte->getLibelle() . '<br>';
+    echo 'marque : ' . $carte->getMarque() . '<br>';
+    echo 'prix hunitaire : ' . $carte->getPrixUnitaire() . '<br>';
+    echo 'stock : ' . $carte->getQteStock() . '<br>';
+    echo 'type de produit : ' . $carte->getType() . '<br>';
+    echo 'Id de la carte postale : ' . $carte->getRefProd() . '<br>';
+    echo '<br>';
+}
+
