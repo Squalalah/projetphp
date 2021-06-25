8805455df6087e6ec29e6c51ddfc584bf1e6ef6f
Squalalah <riquezoune@gmail.com>
2021-06-25 14:36:11 +0200

    Suppression session panier si plus d'objet

--- VIEW/serviceSupprimeLigne.php
+++ VIEW/serviceSupprimeLigne.php
@@ -19,12 +19,12 @@ if(isset($_POST['supprLigne'])||isset($_SESSION['panier'])) {
   $keyLigne=$_POST['supprLigne'];
   $panier=unserialize($_SESSION['panier']);
   /* @var Ligne $ligne */
+  /* @var Panier $panier */
   $ligne = $panier->getLigne($keyLigne);
   DTOProduit::ajouterStock($ligne->getProduit()->getRefProd(), $ligne->getQuantite());
   $panier->supprimeLigne($keyLigne);
-
-
-  $_SESSION['panier']=serialize($panier);
+  if(count($panier->getLignes()) == 0) unset($_SESSION['panier']);
+  else $_SESSION['panier']=serialize($panier);
 }
 
 header('Location:detailPanier.php');
