c205667a2c3155278aadd3900863594ffe29dd36
aureliem07 <aureliemaillot07@gmail.com>
2021-06-25 14:17:24 +0200

    modif css header

--- VIEW/listeProduits.php
+++ VIEW/listeProduits.php
@@ -6,10 +6,12 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="asset/css/listeProduits.css">
 
   <title>Les Pro du I.T.</title>
 </head>
+
+<?php include('header.php'); ?>
 
 <body>
 
