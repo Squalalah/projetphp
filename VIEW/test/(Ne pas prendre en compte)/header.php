1ce650b3988e98dfa93f1e6f5ad5fe577286f93c
aureliem07 <aureliemaillot07@gmail.com>
2021-06-25 13:57:36 +0200

    Ajout Header

---
+++ VIEW/test/(Ne pas prendre en compte)/header.php
@@ -0,0 +1,9 @@
+<header>
+<nav>
+  <div class="topnav">
+    <a class="active" href="index.php">Accueil</a>
+    <a href="listeProduits.php">News</a>
+    <a href="detailPanier.php">Panier</a>
+  </div>
+</nav>
+</header>
