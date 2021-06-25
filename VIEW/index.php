3fbdc9ef93468dd668443b91f986648f69c0056c
Vincent NGUYEN <vngnl@laposte.net>
2021-06-25 14:30:11 +0200

    non mais fait le

--- VIEW/index.php
+++ VIEW/index.php
@@ -5,14 +5,16 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="asset/css/style.css">
+    <link rel="stylesheet" href="asset/css/header.css">
+
     <title>Le site PRO DU I.T.</title>
 </head>
 
 <body>
     <?php
-        include('include/header.php')
+        include('include/header.php');
     ?>
-<h1 class="intro">Bienvenue sur le site<br> <span class="titreSite">Pro </span> <span class="titreSite">Du  I </span>.<span class="titreSite">T</span>.</h1>
+<h1 class="intro">Bienvenue sur le site<br> <span class="titreSite">Pro </span> <span class="titreSite">Du I</span>. <span class="titreSite">T</span>.</h1>
     
 </body>
 </html>
